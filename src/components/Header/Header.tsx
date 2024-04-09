1f0813446aa3b387073f737d7ba0b110196bedc4
Mayya Matueva <mayamatueva@gmail.com>
2024-04-09 17:09:46 +0200

    create header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,24 @@
+import "./Header.css";
+import Girl from "../../assets/girl.svg";
+import Baikal from "../../assets/Baikal.svg";
+import GirlAndBoy from "../../assets/girl_and_boy.svg";
+
+export default function Header() {
+  return (
+    <main>
+      <header className="header">
+        <div className="wrapper">
+          <img src={Girl} alt="Girl" className="girlImage" />
+          <img src={Baikal} alt="Baikal" className="baikalImage" />
+          <div className="name">
+            <p>Байкал Хэлэн</p>
+            <p>Baikal Language</p>
+            <p>Язык Байкала</p>
+          </div>
+          <img src={GirlAndBoy} alt="GirlAndBoy" className="girlAndBoyImage" />
+        </div>
+      </header>
+      <p>Welcome to my platform!</p>
+    </main>
+  );
+}

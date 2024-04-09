6391329c53c909a614f7ea03432150e23afe877e
Mayya Matueva <mayamatueva@gmail.com>
2024-04-09 22:26:59 +0200

    create navigation and add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
 import Home from "../src/pages/Home";
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
     <>
       <Home />
+      <BrowserRouter>
+        <Navigation />
+      </BrowserRouter>
     </>
   );
 }
